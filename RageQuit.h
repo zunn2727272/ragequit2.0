51554f68286b4f407787e6dbb84fc73184a7ea6f
zunn2727272 <levasan03@gmail.com>
2025-07-13 00:01:24 +0500

    Update RageQuit.h

--- RageQuit.h
+++ RageQuit.h
@@ -2,8 +2,6 @@
 #pragma comment(lib, "pluginsdk.lib")
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 
-class RageQuit : public BakkesMod::Plugin::BakkesModPlugin
-void RageQuit::rageQuit_onCommand()
 {
     // Manual command execution
     cvarManager->log("Manual rage quit command executed");
